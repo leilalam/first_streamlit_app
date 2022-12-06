3541061babb2870e33dab88d11128f86ae587f2d
leilalam <119696389+leilalam@users.noreply.github.com>
2022-12-06 13:33:20 -0500

    Update streamlit_app.py
    change api output to json

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,5 @@ streamlit.dataframe(fruits_to_show)
 
 # Display fruityvice api response
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.header('Fruityvice Fruit Advice!')
+streamlit.text(fruityvice_response.json())
