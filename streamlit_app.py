19480d71d95fb7aff7bde62cab7c277eae2c576d
leilalam <119696389+leilalam@users.noreply.github.com>
2022-12-07 11:16:35 -0500

    Update streamlit_app.py
    remove stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,6 @@ if streamlit.button('Get Fruit Load List'):
     my_cnx.close()
     streamlit.dataframe(my_data_rows)
     
-streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
 
 # Function to add a fruit to the fruit list
